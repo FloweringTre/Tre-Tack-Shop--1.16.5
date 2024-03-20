f25ae7e652804b77da2dbf2cf9c7186493feb12a
FloweringTre <lilianna.carlton@gmail.com>
2024-03-20 10:03:20 -0700

    Set up for logging all registries

--- src/main/java/com/kyraltre/tretackshop/TreTackShop.java
+++ src/main/java/com/kyraltre/tretackshop/TreTackShop.java
@@ -1,9 +1,17 @@
 package com.kyraltre.tretackshop;
 
+import com.kyraltre.tretackshop.block.TackShopBlocks;
+import com.kyraltre.tretackshop.registry.AwardShopBlockRegistry;
+import com.kyraltre.tretackshop.registry.AwardShopItems;
+import com.kyraltre.tretackshop.registry.TackShopBlockRegistry;
+import com.kyraltre.tretackshop.registry.TackShopItems;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+//import net.minecraft.client.renderer.ItemBlockRenderTypes;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
@@ -26,59 +34,95 @@ public class TreTackShop
     private static final Logger LOGGER = LogManager.getLogger();
 
     public TreTackShop() {
-        // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-        // Register the enqueueIMC method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
-        // Register the processIMC method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
-        // Register the doClientStuff method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        //ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, TackShopCommonConfigs.SPEC, "tretackshop-common.toml");
+        //TackItems.register(eventBus); // registers the deferred items in ModItems
+        TackShopItems.init(eventBus);
+        TackShopBlockRegistry.init(eventBus);
+        AwardShopBlockRegistry.init(eventBus);
+        AwardShopItems.init(eventBus);
+        TackShopBlocks.register(eventBus); //registers the deferred items in ModBlocks
+
+        eventBus.addListener(this::setup);
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
     }
 
+    private void clientSetup(final FMLClientSetupEvent event) {
+
+//        ItemBlockRenderTypes.setRenderLayer(TackShopBlocks.PUMPKIN_GRAND.get(), RenderType.cutout());
+//        ItemBlockRenderTypes.setRenderLayer(TackShopBlocks.PUMPKIN_RESERVE.get(), RenderType.cutout());
+//        ItemBlockRenderTypes.setRenderLayer(TackShopBlocks.SNOWFLAKE_RESERVE.get(), RenderType.cutout());
+//        ItemBlockRenderTypes.setRenderLayer(TackShopBlocks.SNOWFLAKE_GRAND.get(), RenderType.cutout());
+//        ItemBlockRenderTypes.setRenderLayer(TackShopBlocks.HEART_RESERVE.get(), RenderType.cutout());
+//        ItemBlockRenderTypes.setRenderLayer(TackShopBlocks.HEART_GRAND.get(), RenderType.cutout());
+
+    }
+
+
     private void setup(final FMLCommonSetupEvent event)
     {
         // some preinit code
-        LOGGER.info("HELLO FROM PREINIT");
-        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+        LOGGER.info("Tre says plant a tree <3");
+        LOGGER.info(14 + " Award Sets Loaded.");
     }
 
-    private void doClientStuff(final FMLClientSetupEvent event) {
-        // do something that can only be done on the client
-     //   LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().options);
     }
+//        // Register the setup method for modloading
+//        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+//        // Register the enqueueIMC method for modloading
+//        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
+//        // Register the processIMC method for modloading
+//        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
+//        // Register the doClientStuff method for modloading
+//        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+//
+//        // Register ourselves for server and other game events we are interested in
+//        MinecraftForge.EVENT_BUS.register(this);
+//    }
+//
+//    private void setup(final FMLCommonSetupEvent event)
+//    {
+//        // some preinit code
+//        LOGGER.info("HELLO FROM PREINIT");
+//        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+//    }
+//
+//    private void doClientStuff(final FMLClientSetupEvent event) {
+//        // do something that can only be done on the client
+//     //   LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().options);
+//    }
+//
+//    private void enqueueIMC(final InterModEnqueueEvent event)
+//    {
+//        // some example code to dispatch IMC to another mod
+//        InterModComms.sendTo("tretackshop", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
+//    }
+//
+//    private void processIMC(final InterModProcessEvent event)
+//    {
+//        // some example code to receive and process InterModComms from other mods
+//        LOGGER.info("Got IMC {}", event.getIMCStream().
+//                map(m->m.getMessageSupplier().get()).
+//                collect(Collectors.toList()));
+//    }
+//    // You can use SubscribeEvent and let the Event Bus discover methods to call
+//    @SubscribeEvent
+//    public void onServerStarting(FMLServerStartingEvent event) {
+//        // do something when the server starts
+//        LOGGER.info("HELLO from server starting");
+//    }
+//
+//    // You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
+//    // Event bus for receiving Registry Events)
+//    @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
+//    public static class RegistryEvents {
+//        @SubscribeEvent
+//        public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
+//            // register a new block here
+//            LOGGER.info("HELLO from Register Block");
+//        }
+//    }
 
-    private void enqueueIMC(final InterModEnqueueEvent event)
-    {
-        // some example code to dispatch IMC to another mod
-        InterModComms.sendTo("tretackshop", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
-    }
-
-    private void processIMC(final InterModProcessEvent event)
-    {
-        // some example code to receive and process InterModComms from other mods
-        LOGGER.info("Got IMC {}", event.getIMCStream().
-                map(m->m.getMessageSupplier().get()).
-                collect(Collectors.toList()));
-    }
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
-    @SubscribeEvent
-    public void onServerStarting(FMLServerStartingEvent event) {
-        // do something when the server starts
-        LOGGER.info("HELLO from server starting");
-    }
-
-    // You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
-    // Event bus for receiving Registry Events)
-    @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
-    public static class RegistryEvents {
-        @SubscribeEvent
-        public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
-            // register a new block here
-            LOGGER.info("HELLO from Register Block");
-        }
-    }
-}
